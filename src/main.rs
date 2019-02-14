4928ecc2ee5d6a2b48801777eda5bc0edd64f8a9
Илья Ефимов <inferrna@gmail.com>
2019-02-14 20:15:16 +0800

    Separate functions

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,62 @@ mod imgproc_c;
 use imgproc_c::*;
 use std::ffi::c_void;
 use std::ffi::CString;
+use std::{slice, mem};
 
-use image::DynamicImage;
+use image::{DynamicImage, Rgb};
 const SZ: i32 = 100;
 
 macro_rules! as_ch_ptr {
     ($a:expr) => {CString::new($a).expect("CString::new failed").as_ptr()} 
 }
 
-fn main() {
-    let img: DynamicImage = image::open("/tmp/sample.png").unwrap();
+fn resize_image_with_cv(img: DynamicImage){
+
+}
+
+fn resize_image_cv(cvimg: *mut IplImage, neww: i32, newh: i32) -> *mut IplImage {
+    let cvsize_new = CvSize{width: neww, height: newh};
+    let cvdest = unsafe {cvCreateImage(cvsize_new, (*cvimg).depth, (*cvimg).nChannels)};
+    unsafe {cvResize(cvimg as *mut c_void, cvdest as *mut c_void, CV_INTER_LINEAR as i32)};
+    return cvdest;
+}
+
+fn convert_image_to_cv(img: &DynamicImage) -> *mut IplImage {
     let imgrgb = img.to_rgb();
     let (width, height) = imgrgb.dimensions();
-    println!("Unsafe create ImageHeader with width={} and height={}", width, height);
     let cvsize = CvSize{width: width as i32, height: height as i32};
     let cvimg = unsafe {cvCreateImageHeader(cvsize, IPL_DEPTH_8U as i32, 3)};
-    println!("Unsafe cast pixels to c_void");
     let mut buf = imgrgb.into_vec().into_boxed_slice();
     let data = buf.as_mut_ptr();
-    let bytes_len = buf.len() as i32;
-    println!("{} bytes", bytes_len);
-    println!("Unsafe cvSetData");
     unsafe { cvSetData(cvimg as *mut c_void, data as *mut c_void, (*cvimg).widthStep) };
+    return cvimg;
+}
+
+fn convert_image_from_cv(cvimg: *mut IplImage) -> DynamicImage {
+    let (width, height) = unsafe {((*cvimg).width as u32, (*cvimg).height as u32)};
+    let vbytes = unsafe { slice::from_raw_parts((*cvimg).imageDataOrigin, (3*width*height) as usize).to_vec()};
+    let vuchar = unsafe {mem::transmute::<Vec<i8>, Vec<u8>>(vbytes)};
+    let imgbuf = image::ImageBuffer::from_vec(width, height, vuchar).unwrap();
+    return image::ImageRgb8(imgbuf);
+}
+
+fn main() {
+    let img: DynamicImage = image::open("/tmp/sample.png").unwrap();
+    println!("Unsafe create cv image");
+    let cvimg = convert_image_to_cv(&img);
     
-    println!("Unsafe save original file");
-    unsafe { cvSaveImage(as_ch_ptr!("/tmp/sample_fromcv.jpg"), cvimg as *mut c_void, &0i32) };
+    /*println!("Unsafe save original file");
+    unsafe { cvSaveImage(as_ch_ptr!("/tmp/sample_fromcv.jpg"), cvimg as *mut c_void, &0i32) };*/
     
-    let cvsize_new = CvSize{width: SZ, height: SZ};
-    println!("Unsafe create empty buf");
-    let cvdest = unsafe {cvCreateImage(cvsize_new, IPL_DEPTH_8U as i32, 3)};
-    println!("Unsafe cvResize. CV_INTER_LINEAR = {}", CV_INTER_LINEAR);
-    unsafe {cvResize(cvimg as *mut c_void, cvdest as *mut c_void, CV_INTER_LINEAR as i32)};
+    println!("Unsafe resize image");
+    let cvdest = resize_image_cv(cvimg, SZ, SZ);
     
-    println!("Unsafe save resized file");
+    println!("Unsafe save resized image");
     unsafe { cvSaveImage(as_ch_ptr!("/tmp/resized_sample_fromcv.jpg"), cvdest as *mut c_void, &0i32) };
+    
+    println!("Unsafe convert resized image back");
+    let imgdest = convert_image_from_cv(cvdest);
+    imgdest.save("/tmp/resized_sample_fromimg.jpg").unwrap();
 
     println!("Hello, world!");
 }
